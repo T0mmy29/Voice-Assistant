9725025e7108bd0f00755db764f287da7b73e276
Soumith <91047727+T0mmy29@users.noreply.github.com>
2022-06-04 21:32:13 +0530

    Main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+from email.mime import application
+from logging import shutdown
 import speech_recognition as sr
 import pyjokes
 import pyttsx3
 import wikipedia
 import webbrowser
 import os
+import smtplib
 import ctypes
+import time
 import win32com.client as wincl
 import datetime
 import pywhatkit
+import subprocess
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
@@ -31,6 +36,15 @@ def take_input():
         pass
     return command
 
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+     
+    # Enable low security in gmail
+    server.login('your email id', 'your email password')
+    server.sendmail('your email id', to, content)
+    server.close()
 
 
 def wishMe():
@@ -79,14 +93,49 @@ if __name__ == '__main__':
         elif 'lock device' in query:
             talk('Locking the screen')
             ctypes.windll.user32.LockWorkStation()
-
-
+        elif 'send a mail' in query:
+            try:
+                talk("What should I say?")
+                content = take_input()
+                talk("whome should i send")
+                to = input()   
+                sendEmail(to, content)
+                talk("Email has been sent !")
+            except Exception as e:
+                print(e)
+                talk('I am not able to send this mail.')
+       
+        elif 'search' in query or 'play' in query:
+            query = query.replace("search", "")
+            query = query.replace("play", "")         
+            webbrowser.open(query)
         
+        elif 'open' in query:
+            query = query.replace('open','')
+            talk('Opening'+ query)
+            os.system(query)
+        
+        elif 'shutdown' in query:
+            talk('Hold on! Shutting down the device')
+            subprocess.call('shutdown / p /f')
 
+        elif "don't listen" or 'stop listening' in query:
+            talk('okay, but for how much time do you want me to shut up?')
+            a = int(take_input())
+            time.sleep(a)
+            print(a) 
 
-        elif 'turn off' in query:
+        elif 'restart the device' in query:
+            talk('Restarting the system')
+            subprocess.call(['shutdown','/r'])        
+            
+        elif 'turn off' or 'exit' in query:
             talk('Turning off')
             exit()
         
+
+        
+
+
 
 
